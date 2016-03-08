c1e801efb38eb5df2f5f1502bb1c4991b62923b6
Jorge Naranjo <j.naranjojr@gmail.com>
2016-03-07 19:26:26 -0500

    Add flash notice

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+class HomeController < ApplicationController
+
+  def index
+    if current_user
+      redirect_to posts_path
+    end
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  
   devise_for :users
+
+  root "home#index"
+
   resources :posts
 
-  root "posts#index"
-  
 end
